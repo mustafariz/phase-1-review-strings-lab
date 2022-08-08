b1abea7701b61317dee76af4393b483ec5df06ba
syed mustafa ali rizvi <mustafariz15@gmail.com>
2022-08-07 19:37:03 -0700

    commit tree 16ae85827a50783ed181b4ec430aa0e67e601bbf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'bug';
+
+
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+
+
+ let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!` 
+
+ 
